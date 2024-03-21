95dc4fb4c01d18b64552fa9af0740e86d6ff74e9
ShatadalDas <shatadaldas2020@gmail.com>
2024-03-21 18:38:17 +0530

    chore: first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Steps to start contribution
+
+- Folk this repo
+- Create Python virtual environment and Activate, take help from this [link](https://www.freecodecamp.org/news/how-to-setup-virtual-environments-in-python)
+
+   *Before proceeding, check the name you gave to the virtual environment, if that exists or not in the .gitignore file, if not then write the name in a new line*
+
+- After activating, run this command `pip install -r requirements.txt` in the terminal of this folder
+- Then run `python main.py`
+
+   *The Project is running now..!*
+
+- Make changes and create a Pull Request
+- If you install any new package then don't forget to run `pip freeze > requirements.txt` and pushing it to github, before making any PR.
+
+    *Note:* Before making any commit, refer to this [link](https://gist.github.com/qoomon/5dfcdf8eec66a051ecd85625518cfd13), this link tells you about the format of commit message
---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+from flask_cors import CORS
+from math import ceil
+import io
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/compress/image/banner", methods = ["POST"])
+def compress():
+  if request.method == "POST":
+    if 'file' not in request.files:
+      print("Couldn't resolve file...!?")
+      return jsonify({"message": "file not found"})
+    try:
+      file = request.files['file']
+      img = Image.open(file)
+      factor = 1.05
+      resizedImg = img
+      width, height = resizedImg.size
+
+      maxSide = max(width, height)
+
+      while maxSide > 700:
+        width, height = resizedImg.size
+        maxSide = max(width, height)
+        resizedImg = resizedImg.resize((int(width//factor), int(height//factor)))
+
+      buffered = io.BytesIO()
+      resizedImg.save(buffered, format="jpeg", quality=70)
+      print("final filesize:", ceil(buffered.tell()/1024), "kb")
+      print(buffered.getvalue())
+      compImg = Image.open(buffered)
+
+      compImg.save("compressed-" + file.filename)
+      
+      response = jsonify({"message": "Done"})
+      print("Compressed Successfully : )")
+      return response, 200
+    
+    except RuntimeError as e:
+      response = jsonify({"message": "Something went wrong!"})
+      return response, 500
+  else:
+      response = jsonify({"message": "Expected a POST request"})
+      
+      return response, 400
+
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
